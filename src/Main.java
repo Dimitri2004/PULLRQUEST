fe494f11b4e504080fd9c80a824d8159d38244f1
Dimitri2004 <dapariciopereira@danielcastelao.org>
2025-01-24 09:22:02 +0100

    JAva hello World (#1)
    * exp2 1
    
    * exp2 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a Java program.");
+        System.out.println("I'm learning Java!");
+        int number = 5;
     }
 }
